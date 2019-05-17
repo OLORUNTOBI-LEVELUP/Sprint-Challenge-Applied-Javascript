7f6ca4f9188fcf721d40bc16da7d34dafa08d3dc
oloruntobi <awoderuoloruntobi@gmail.com>
2019-05-17 10:38:18 +0100

    Added functionality to the carousel.js I added functionality to the carousel making the pictures appear/dissapear when the right or left button is clicked thereby also completing the first stretch goal

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -1,8 +1,61 @@
 class Carousel {
+    constructor(element) {
+      this.element = element
+  
+      // DOM References
+      this.rightButton = document.querySelector(".right-button")
+      this.leftButton = document.querySelector(".left-button")
+      this.images = document.querySelectorAll(".carousel img") // this.images returns a nodeList
+  
+    
+      this.rightButton.addEventListener("click", () => this.rightClick())
+      this.leftButton.addEventListener("click", () => this.leftClick())
+  
+  
+      this.images[0].style.display = "block"
+      this.currentIndex = 0
+  
+      this.images.forEach((image, index) =>
+        image.setAttribute("data-index", index)
+      )
 
-}
+      this.length = this.images.length - 1
+    }
+  
+    // Methods
+    rightClick() {  
+      if (this.currentIndex < this.length) { 
+      this.currentIndex += 1
+  
+      let newImage = document.querySelector(`.carousel img[data-index="${this.currentIndex}"]`)
+      let previousImage = document.querySelector(`.carousel img[data-index="${this.currentIndex - 1}"]`)
+  
+  
+      newImage.style.display = "block"
+      previousImage.style.display = "none"
+      }
+   
+    }
+  
+    leftClick() {
+      if (this.currentIndex > 0) {
+    
+      this.currentIndex -= 1
+  
+      let newImage = document.querySelector(`.carousel img[data-index="${this.currentIndex}"]`)
+      let previousImage = document.querySelector(`.carousel img[data-index="${this.currentIndex + 1}"]`)
+  
+      newImage.style.display = "block"
+      previousImage.style.display = "none"
+    }
+  }
+  }
+  
+  let carousel = document.querySelector(".carousel")
+  carousel = new Carousel(carousel)
+  
+  
 
-let carousel = document.querySelector();
 
 /* If You've gotten this far, you're on your own! Although we will give you some hints:
     1. You will need to grab a reference to the carousel, and in it grab the left and right buttons
